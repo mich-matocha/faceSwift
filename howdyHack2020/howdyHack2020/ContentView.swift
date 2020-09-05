4a8b3fe99d234d3e14d29558ee3735b27ec658da
Michaela Matocha <mich-matocha@users.noreply.github.com>
2020-09-05 13:18:57 -0500

    Init

---
+++ howdyHack2020/howdyHack2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  howdyHack2020
+//
+//  Created by Michaela Matocha on 9/5/20.
+//  Copyright © 2020 Michaela Matocha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
